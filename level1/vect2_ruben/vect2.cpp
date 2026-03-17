2a6698e73eedb5389f185d817dc60522f65e9f6f
higrub89 <higuitaruben@hotmail.com>
2026-03-17 13:19:53 +0100

    Exam-05 Vect2 versin final

--- level1/vect2_ruben/vect2.cpp
+++ level1/vect2_ruben/vect2.cpp
@@ -1,49 +1,97 @@
 #include "vect2.hpp"
 
 vect2& vect2::operator=(const vect2& rhs) {
-    if (this != &rhs) {
+    if (this != &rhs)
+    {
         _x = rhs._x;
         _y = rhs._y;
     }
     return *this;
 }
 
-int& vect2::operator[](int i) { return (i == 0) ? _x : _y; }
-const int& vect2::operator[](int i) const {return (i == 0) ? _x : _y; }
-
-vect2& vect2::operator+=(const vect2& rhs) {
-    _x += rhs._x; _y += rhs._y;
-    return *this;
+ int& vect2::operator[](int i) {
+    if (i == 0)
+        return _x;
+    return _y;
 }
 
-vect2& vect2::operator-=(const vect2& rhs) {
-    _x -= rhs._x; _y -= rhs._y;
-    return *this;
+const int& vect2::operator[](int i) const  {
+    if (i == 0)
+        return _x;
+    return _y;
 }
 
-vect2& vect2::operator*=(int s) {
-    _x *= s; _y *= s;
-    return *this;
-}
 
 vect2 vect2::operator-() const {
     return vect2(-_x, -_y);
 }
 
-vect2& vect2::operator++() { return *this += vect2(1, 1); }
-vect2 vect2::operator++(int) { vect2 tmp(*this); ++(*this); return tmp; }
-vect2& vect2::operator--() { return *this -= vect2(1, 1); }
-vect2 vect2::operator--(int) { vect2 tmp(*this); --(*this); return tmp; }
+vect2& vect2::operator++() {
+    _x++;
+    _y++;
+    return *this;
+}
 
-bool vect2::operator==(const vect2& rhs) const { return _x == rhs._x && _y == rhs._y; }
-bool vect2::operator!=(const vect2& rhs) const { return !(*this == rhs); }
+vect2 vect2::operator++(int) {
+    vect2 tmp(*this);
+    ++(*this);
+    return tmp;
+}
 
-vect2 operator+(vect2 lhs, const vect2& rhs) { return lhs += rhs; }
-vect2 operator-(vect2 lhs, const vect2& rhs) { return lhs -= rhs; }
-vect2 operator*(vect2 lhs, int scalar)       { return lhs *= scalar; }
-vect2 operator*(int scalar, vect2 rhs)       { return rhs *= scalar; }
+vect2& vect2::operator--() {
+    return *this -= vect2(1, 1);
+}
+
+vect2 vect2::operator--(int) {
+    vect2 tmp(*this);
+    --(*this);
+    return tmp;
+}
+
+vect2& vect2::operator+=(const vect2& rhs) {
+    _x += rhs._x;
+    _y += rhs._y;
+    return *this;
+}
+
+vect2& vect2::operator-=(const vect2& rhs) {
+    _x -= rhs._x;
+    _y -= rhs._y;
+    return *this;
+}
+
+vect2& vect2::operator*=(int s) {
+    _x *= s;
+    _y *= s;
+    return *this;
+}
+
+bool vect2::operator==(const vect2& rhs) const {
+    return _x == rhs._x && _y == rhs._y;
+}
+
+bool vect2::operator!=(const vect2& rhs) const {
+    return !(*this == rhs);
+}
+
+vect2 operator+(vect2 lhs, const vect2& rhs) {
+    return lhs += rhs;
+}
+
+vect2 operator-(vect2 lhs, const vect2& rhs) {
+    return lhs -= rhs;
+}
+
+vect2 operator*(vect2 lhs, int scalar) {
+    return lhs *= scalar;
+}
+
+vect2 operator*(int scalar, vect2 rhs) {
+    return rhs *= scalar;
+}
 
 std::ostream& operator<<(std::ostream& os, const vect2& v) {
-    os << "{" << v[0] << ", " << v[1] << "}";
-    return os;
+     os << "{" << v[0] << ", " << v[1] << "}";
+     return os;
 }
+
--- level1/vect2_ruben/vect2.hpp
+++ level1/vect2_ruben/vect2.hpp
@@ -7,16 +7,16 @@ class vect2 {
     private:
         int _x;
         int _y;
-    
     public:
         vect2(int x = 0, int y = 0) : _x(x), _y(y) {}
         vect2(const vect2& src) : _x(src._x), _y(src._y) {}
         ~vect2() {}
-
+        
         vect2& operator=(const vect2& rhs);
-        int& operator[](int index);
-        const int& operator[](int index) const;
 
+        int& operator[](int i);
+        const int& operator[](int i) const;
+        
         vect2 operator-() const;
         vect2& operator++();
         vect2 operator++(int);
@@ -30,12 +30,12 @@ class vect2 {
         bool operator==(const vect2& rhs) const;
         bool operator!=(const vect2& rhs) const;
 
-        int getX() const {return _x;}
-        int getY() const {return _y;}
+        int getX() const { return _x; }
+        int getY() const { return _y; }
 };
 
-vect2 operator+(vect2 lhs, const vect2& rhs);
-vect2 operator-(vect2 lhs, const vect2& rhs);
+vect2 operator+(vect2 lhs, const  vect2& rhs);
+vect2 operator-(vect2 lhs, const  vect2& rhs);
 vect2 operator*(vect2 lhs, int scalar);
 vect2 operator*(int scalar, vect2 rhs);
 
